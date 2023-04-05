d63523980ecb68f190ad8e138b77c5c26ab6954f
vadym.oselskyi <vadym.oselskyi@skai.io>
2023-04-05 23:53:04 +0300

    test commit

--- historical-data/src/main/java/io/skai/HistoricalData.java
+++
@@ -1,7 +0,0 @@
-package io.skai;
-
-public class HistoricalData {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ historical-data/src/main/java/io/skai/HistoricalDataApplication.java
@@ -0,0 +1,11 @@
+package io.skai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistoricalDataApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HistoricalDataApplication.class, args);
+    }
+}
