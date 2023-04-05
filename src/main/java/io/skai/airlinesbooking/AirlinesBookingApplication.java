7f45218dd4e1f3a7d1d4e6146ff5d01eee4ffc39
vadym.oselskyi <vadym.oselskyi@skai.io>
2023-04-05 22:56:30 +0300

    Initial commit

---
+++ src/main/java/io/skai/airlinesbooking/AirlinesBookingApplication.java
@@ -0,0 +1,13 @@
+package io.skai.airlinesbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlinesBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlinesBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/skai/airlinesbooking/AirlinesBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package io.skai.airlinesbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlinesBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
