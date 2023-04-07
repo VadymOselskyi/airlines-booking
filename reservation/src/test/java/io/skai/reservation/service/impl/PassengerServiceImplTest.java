2e4526cceeae896455a55dba906778371933949a
vadym.oselskyi <vadym.oselskyi@skai.io>
2023-04-07 15:00:48 +0300

    add test for getByEmail method

--- reservation/src/test/java/io/skai/reservation/service/impl/PassengerServiceImplTest.java
+++ reservation/src/test/java/io/skai/reservation/service/impl/PassengerServiceImplTest.java
@@ -1,5 +1,7 @@
 package io.skai.reservation.service.impl;
 
+import io.skai.reservation.dto.PassengerDto;
+import io.skai.reservation.jooq.tables.pojos.Passenger;
 import io.skai.reservation.mapper.PassengerMapper;
 import io.skai.reservation.repository.PassengerRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,7 +11,8 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import static org.mockito.Mockito.*;
-
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 class PassengerServiceImplTest {
 
     @InjectMocks
@@ -25,17 +28,32 @@ class PassengerServiceImplTest {
     }
 
     @Test
-    void WhenCreateOnePassengerInsertWillWorkOnce() {
+    void WhenCreateOnePassengerInsertWillWorkOnceTest() {
         passengerService.create(any());
 
         verify(passengerRepository, times(1)).insert(any());
     }
     @Test
-    void WhenCreateTwoPassengerInsertWillWorkTwice() {
+    void WhenCreateTwoPassengerInsertWillWorkTwiceTest() {
         passengerService.create(any());
         passengerService.create(any());
 
         verify(passengerRepository, times(2)).insert(any());
     }
 
+    @Test
+    void WhenGetByPassengerByEmailShouldReturnPassengerDtoTest(){
+        Passenger passenger = new Passenger(1L, "Vadim", "Oselskyi", "vo@gmail.com", "+3806834232");
+        PassengerDto passengerDto = new PassengerDto(1L, "Vadim", "Oselskyi", "vo@gmail.com", "+3806834232");
+
+        when(passengerRepository.selectPassenger(anyString())).thenReturn(passenger);
+        when(passengerMapper.passengerTOPassengerDto(any())).thenReturn(passengerDto);
+        PassengerDto passengerByEmail = passengerService.getPassengerByEmail(anyString());
+
+        assertThat(passengerByEmail.id(), equalTo(passenger.getId()));
+        assertThat(passengerByEmail.firstName(), equalTo(passenger.getFirstName()));
+        assertThat(passengerByEmail.lastName(), equalTo(passenger.getLastName()));
+        assertThat(passengerByEmail.email(), equalTo(passenger.getEmail()));
+        assertThat(passengerByEmail.phone(), equalTo(passenger.getPhone()));
+    }
 }
