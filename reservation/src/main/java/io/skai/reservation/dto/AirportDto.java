05e4a8721c01ff2e8ac37ba94d8032f17de023f6
vadym.oselskyi <vadym.oselskyi@skai.io>
2023-04-11 22:02:49 +0300

    add redis and test cashed methods

--- reservation/src/main/java/io/skai/reservation/dto/AirportDto.java
+++ reservation/src/main/java/io/skai/reservation/dto/AirportDto.java
@@ -1,9 +1,9 @@
 package io.skai.reservation.dto;
 
-public record AirportDto(
-        Long id,
-        String name,
-        String countryCode,
-        String city
-) {
+import java.io.Serializable;
+
+public record AirportDto(Long id,
+                         String name,
+                         String countryCode,
+                         String city) implements Serializable {
 }
---
+++ reservation/src/test/java/io/skai/reservation/service/impl/AirportServiceImplCashedTest.java
@@ -0,0 +1,51 @@
+package io.skai.reservation.service.impl;
+
+import io.skai.reservation.jooq.tables.pojos.Airport;
+import io.skai.reservation.repository.AirportRepository;
+import io.skai.reservation.service.AirportService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+class AirportServiceImplCashedTest {
+
+    private final Airport LVIV_AIRPORT = new Airport(1L, "west", "UA", "Lviv");
+
+    @MockBean
+    private AirportRepository airportRepository;
+
+    @Autowired
+    private AirportService airportService;
+
+    @Test
+    void testCasedMethodGet() {
+
+        when(airportRepository.selectOneById(LVIV_AIRPORT.getId())).thenReturn(LVIV_AIRPORT);
+
+        airportService.getAirport(LVIV_AIRPORT.getId());
+        airportService.getAirport(LVIV_AIRPORT.getId());
+
+        verify(airportRepository, atMostOnce())
+                .selectOneById(LVIV_AIRPORT.getId());
+
+    }
+
+    @Test
+    void testCasedMethodGetAll() {
+
+        when(airportRepository.selectAll()).thenReturn(List.of(LVIV_AIRPORT));
+
+        airportService.getAllAirports();
+        airportService.getAllAirports();
+
+        verify(airportRepository, atMostOnce())
+                .selectAll();
+
+    }
+}
