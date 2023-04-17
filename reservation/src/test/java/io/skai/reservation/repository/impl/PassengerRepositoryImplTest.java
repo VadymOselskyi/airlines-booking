3819abc1b142c273bccaaced0ab4a5e2abc5310c
vadym.oselskyi <vadym.oselskyi@skai.io>
2023-04-17 10:30:00 +0300

    update test

--- reservation/src/test/java/io/skai/reservation/repository/impl/PassengerRepositoryImplTest.java
+++ reservation/src/test/java/io/skai/reservation/repository/impl/PassengerRepositoryImplTest.java
@@ -3,7 +3,6 @@ package io.skai.reservation.repository.impl;
 import io.skai.reservation.BaseApplicationContextTest;
 import io.skai.reservation.jooq.tables.pojos.Passenger;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static io.skai.reservation.jooq.Tables.PASSENGER;
@@ -12,16 +11,7 @@ import static org.hamcrest.Matchers.*;
 
 class PassengerRepositoryImplTest extends BaseApplicationContextTest {
 
-    private Passenger passenger;
-
-    @BeforeEach
-    void setUp() {
-        passenger = new Passenger();
-        passenger.setEmail("vo@gmail.com");
-        passenger.setFirstName("Vadim");
-        passenger.setLastName("Oselskyi");
-        passenger.setPhone("097234395");
-    }
+    private final Passenger passenger = new Passenger(1L, "vo@gmail.com", "Vadim", "Oselskyi", "097234395");
 
     @AfterEach
     void tearDown() {
