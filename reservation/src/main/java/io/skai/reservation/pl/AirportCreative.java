baa99f99f778e9025b0d059d9005a2b573d1c36c
VolodymyrM <volodymyr.mudryk@skai.io>
2023-04-19 11:55:57 +0300

    replaced deprecated createField method

--- reservation/src/main/java/io/skai/reservation/pl/AirportCreative.java
+++ reservation/src/main/java/io/skai/reservation/pl/AirportCreative.java
@@ -3,6 +3,7 @@ package io.skai.reservation.pl;
 import com.kenshoo.jooq.AbstractDataTable;
 import org.jooq.Record;
 import org.jooq.TableField;
+import org.jooq.impl.DSL;
 import org.jooq.impl.SQLDataType;
 
 public class AirportCreative extends AbstractDataTable<AirportCreative> {
@@ -14,9 +15,9 @@ public class AirportCreative extends AbstractDataTable<AirportCreative> {
     }
 
     public final TableField<Record, Long> id = createPKField("id", SQLDataType.BIGINT.identity(true));
-    public final TableField<Record, String> name = createField("name", SQLDataType.VARCHAR(255), TABLE);
-    public final TableField<Record, String> country_code = createField("country_code", SQLDataType.VARCHAR.length(255).nullable(false));
-    public final TableField<Record, String> city = createField("city", SQLDataType.VARCHAR.length(255).nullable(false));
+    public final TableField<Record, String> name = createField(DSL.name("name"), SQLDataType.VARCHAR(255), TABLE);
+    public final TableField<Record, String> country_code = createField(DSL.name("country_code"), SQLDataType.VARCHAR.length(255).nullable(false));
+    public final TableField<Record, String> city = createField(DSL.name("city"), SQLDataType.VARCHAR.length(255).nullable(false));
 
     @Override
     public AirportCreative as(String alias) {
