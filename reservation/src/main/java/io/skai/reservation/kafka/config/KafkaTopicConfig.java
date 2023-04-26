a86026c872c4e24b3510dd5fb802f6a0c4a54853
vadym.oselskyi <vadym.oselskyi@skai.io>
2023-04-26 14:00:18 +0300

    SEARCH-0003 refactoring

--- reservation/src/main/java/io/skai/reservation/kafka/config/KafkaTopicConfig.java
+++ reservation/src/main/java/io/skai/reservation/kafka/config/KafkaTopicConfig.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.KafkaAdmin;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
@@ -22,9 +21,7 @@ public class KafkaTopicConfig {
 
     @Bean
     public KafkaAdmin kafkaAdmin() {
-        Map<String, Object> configs = new HashMap<>();
-        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-        return new KafkaAdmin(configs);
+        return new KafkaAdmin(Map.of(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress));
     }
 
     @Bean
