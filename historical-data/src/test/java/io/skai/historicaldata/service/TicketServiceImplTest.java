9c6bff3c9654e2d23c82fc09594f8f75f6c9b75b
vadym.oselskyi <vadym.oselskyi@skai.io>
2023-04-28 10:33:52 +0300

    SEARCH-0003 refactoring

--- historical-data/src/test/java/io/skai/historicaldata/service/TicketServiceImplTest.java
+++ historical-data/src/test/java/io/skai/historicaldata/service/TicketServiceImplTest.java
@@ -1,7 +1,6 @@
 package io.skai.historicaldata.service;
 
 import io.skai.historicaldata.dto.HistoricalTicketDto;
-import io.skai.historicaldata.exception.TicketWasNotSavedException;
 import io.skai.historicaldata.mapper.TicketMapper;
 import io.skai.historicaldata.model.HistoricalTicket;
 import io.skai.historicaldata.repository.TicketRepository;
@@ -13,64 +12,55 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 class TicketServiceImplTest {
 
-    private final HistoricalTicket TICKET =
-            new HistoricalTicket(1L, "FName", "LName", "email@gmail.com",
-                    "123", "B12", new BigDecimal("123.00"),
-                    LocalDateTime.of(2023, 4, 17, 14, 20),
-                    "arrAir", "UA", "Kyiv",
-                    LocalDateTime.of(2023, 4, 17, 21, 35),
-                    "arrAir", "GB", "London");
-    private final HistoricalTicketDto TICKETDTO =
-            new HistoricalTicketDto(1L, "FName", "LName", "email@gmail.com",
-                    "123", "B12", new BigDecimal("123.00"),
-                    LocalDateTime.of(2023, 4, 17, 14, 20),
-                    "arrAir", "UA", "Kyiv",
-                    LocalDateTime.of(2023, 4, 17, 21, 35),
-                    "arrAir", "GB", "London");
+    private static final HistoricalTicket TICKET = createHistoricalTicket();
+    private static final HistoricalTicketDto TICKET_DTO = createHistoricalTicketDto();
 
     private final TicketRepository ticketRepository = Mockito.mock(TicketRepository.class);
     private final TicketMapper ticketMapper = Mockito.mock(TicketMapper.class);
-
     private final TicketServiceImpl ticketService = new TicketServiceImpl(ticketRepository, ticketMapper);
 
     @Test
     void whenDbHasTicketsWithTheSameEmailThenGetTicketsReturnTheir() {
-
-        when(ticketMapper.historicalTicketToHistoricalTicketDto(TICKET)).thenReturn(TICKETDTO);
+        when(ticketMapper.historicalTicketToHistoricalTicketDto(TICKET)).thenReturn(TICKET_DTO);
         when(ticketRepository.findAll(TICKET.getEmail())).thenReturn(List.of(TICKET));
 
         List<HistoricalTicketDto> tickets = ticketService.getTickets(TICKET.getEmail());
 
-        assertThat(tickets)
-                .hasSize(1)
-                .contains(TICKETDTO);
-
+        assertThat(tickets).containsExactly(TICKET_DTO);
         verify(ticketRepository).findAll(TICKET.getEmail());
         verify(ticketMapper).historicalTicketToHistoricalTicketDto(TICKET);
     }
 
     @Test
     void whenCreateOneTicketThenTicketRepositoryShouldInsertOnce() {
+        when(ticketMapper.historicalTicketDtoToHistoricalTicket(TICKET_DTO)).thenReturn(TICKET);
 
-        when(ticketMapper.historicalTicketDtoToHistoricalTicket(TICKETDTO)).thenReturn(TICKET);
-
-        ticketService.saveTicket(TICKETDTO);
+        ticketService.saveTicket(TICKET_DTO);
 
         verify(ticketRepository).save(TICKET);
-        verify(ticketMapper).historicalTicketDtoToHistoricalTicket(TICKETDTO);
+        verify(ticketMapper).historicalTicketDtoToHistoricalTicket(TICKET_DTO);
     }
 
-    @Test
-    void whenInputDtoIsNullThenThrowException() {
-        when(ticketRepository.save(null)).thenThrow(new IllegalArgumentException());
+    private static HistoricalTicket createHistoricalTicket() {
+        return new HistoricalTicket(1L, "FName", "LName", "email@gmail.com",
+                "123", "B12", new BigDecimal("123.00"),
+                LocalDateTime.of(2023, 4, 17, 14, 20),
+                "arrAir", "UA", "Kyiv",
+                LocalDateTime.of(2023, 4, 17, 21, 35),
+                "arrAir", "GB", "London");
+    }
 
-        assertThatThrownBy(() -> ticketService.saveTicket(null))
-                .isInstanceOf(TicketWasNotSavedException.class)
-                .hasMessage("Ticket is null or uses optimistic locking");
+    private static HistoricalTicketDto createHistoricalTicketDto() {
+        return new HistoricalTicketDto(1L, "FName", "LName", "email@gmail.com",
+                "123", "B12", new BigDecimal("123.00"),
+                LocalDateTime.of(2023, 4, 17, 14, 20),
+                "arrAir", "UA", "Kyiv",
+                LocalDateTime.of(2023, 4, 17, 21, 35),
+                "arrAir", "GB", "London");
     }
 }
