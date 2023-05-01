30c01161b72b9e2a1921ae0e541175a4b5f9041a
vadym.oselskyi <vadym.oselskyi@skai.io>
2023-05-01 14:24:04 +0300

    SEARCH-0003 refactoring

--- reservation/src/main/java/io/skai/reservation/kafka/KafkaProducer.java
+++ reservation/src/main/java/io/skai/reservation/kafka/KafkaProducer.java
@@ -1,7 +1,7 @@
 package io.skai.reservation.kafka;
 
+import io.skai.reservation.dto.HistoryTicketDto;
 import io.skai.reservation.kafka.config.KafkaProperties;
-import io.skai.reservation.service.DbArchiveService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
@@ -11,10 +11,9 @@ import org.springframework.stereotype.Component;
 public class KafkaProducer {
 
     private final KafkaTemplate<String, Object> kafkaTemplate;
-    private final DbArchiveService dbArchiveService;
     private final KafkaProperties properties;
 
-    public void sendMessage() {
-        dbArchiveService.getForArchive().forEach(dto -> kafkaTemplate.send(properties.getTopic() , dto));
+    public void sendMessage(HistoryTicketDto ticketDto) {
+        kafkaTemplate.send(properties.getTopic(), ticketDto);
     }
 }
--- reservation/src/main/java/io/skai/reservation/kafka/config/KafkaProperties.java
+++ reservation/src/main/java/io/skai/reservation/kafka/config/KafkaProperties.java
@@ -12,4 +12,5 @@ import org.springframework.context.annotation.Configuration;
 public class KafkaProperties {
     private String topic;
     private String bootStrapServers;
+    private String groupId;
 }
